429cd7882dac25734f9a7abc1de8edd55c64fc6a
Isaline <isalindta@gmail.om>
2021-10-17 14:59:29 +0200

    🎉 :construction: Start project with structure & working on queries

---
+++ queries/queries.sql
@@ -0,0 +1,9 @@
+-- Ajouter un patient à la base
+
+INSERT INTO patient (ID, NOM, PRENOM, AGE, SEXE) VALUES (1, "Tic", "Tac", 24, 0);
+-- Rajouter des 'insert' selon les différents cas possibles (par exemple si des valeurs 
+-- son facultatives)
+
+
+-- Modifier le profil d'expression transcriptomique d'un patient
+
